58c6365fa0d86185c4de387f4b560476e02f67f1
Velaztiqui Trigo Mauro Nicolas <trigotex@gmail.com>
2024-04-09 19:20:54 -0300

    TP2 Terminado

--- tp2_4.c
+++ tp2_4.c
@@ -1,50 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-    struct{
+struct{
         int velocidad;
         int anio;
         int cantidad;
         char *tipo_cpu;
     } typedef compu;
 
-    compu misComputadoras[5];
+void mostrarLista(compu l[]);
+void mostrarPcMasVieja(compu l[]);
+void mostrarPcMasRapida(compu l[]);
 
+int main(){
+
+    char tipos[5][10] = {"AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+    compu misComputadoras[5] = {
+        {1, 2017, 2, tipos[rand() % 5]},
+        {2, 2020, 5, tipos[rand() % 5]},
+        {3, 2016, 7, tipos[rand() % 5]},
+        {2, 2023, 4, tipos[rand() % 5]},
+        {1, 2022, 8, tipos[rand() % 5]}
+    };
+
+    mostrarLista(misComputadoras);
+
+    mostrarPcMasVieja(misComputadoras);
+
+    mostrarPcMasRapida(misComputadoras);
+
+}
+
+void mostrarLista(compu l[]){
 
     for(int i = 0; i < 5; i++){
 
-    printf("Ingrese los datos de la computadora %d\n", i);
-        
-    do
-    {
-        printf("Velocidad: ");
-        fflush(stdin);
-        scanf("%d", misComputadoras[i].velocidad);
-    } while (misComputadoras[i].velocidad < 1 && misComputadoras[i].velocidad > 3);
-    
-    
-    do
-    {
-        printf("Anio: ");
-        fflush(stdin);
-        scanf("%d", misComputadoras[i].anio);
-    } while (misComputadoras[i].anio < 2015 && misComputadoras[i].anio > 2023);
-    
+        printf("\nDatos de Computadora %d",  i + 1);
+        printf("\nVelocidad: %d GHz", l[i].velocidad);
+        printf("\nAnio: %d", l[i].anio);
+        printf("\nCantidad: %d", l[i].cantidad);
+        printf("\nTipo de CPU: %s\n", l[i].tipo_cpu);
 
-    do
-    {
-        printf("Cantidad: ");
-        fflush(stdin);
-        scanf("%d", misComputadoras[i].cantidad);
-    } while (misComputadoras[i].cantidad < 1 && misComputadoras[i].cantidad > 8);
+    }
 
-    
-    
+}
 
-    printf("Tipo de CPU: ");
-    fflush(stdin);
+void mostrarPcMasVieja(compu l[]){
 
+    int menor = 2023;
+
+    for(int i = 0; i < 5; i++){
+        if(l[i].anio < menor){
+            menor = l[i].anio;
+        }
+    }
+
+    for(int i = 0; i < 5; i++){
+        if(l[i].anio == menor){
+            printf("\nDatos de Computadora Mas Vieja");
+            printf("\nVelocidad: %d GHz", l[i].velocidad);
+            printf("\nAnio: %d", l[i].anio);
+            printf("\nCantidad: %d", l[i].cantidad);
+            printf("\nTipo de CPU: %s\n", l[i].tipo_cpu);
+        }
+    }
+}
+
+void mostrarPcMasRapida(compu l[]){
+
+    int mayor = 0;
+
+    for(int i = 0; i < 5; i++){
+        if(l[i].velocidad > mayor){
+            mayor = l[i].velocidad;
+        }
+    }
+
+    for(int i = 0; i < 5; i++){
+        if(l[i].velocidad == mayor){
+            printf("\nDatos de Computadora Mas Rapida");
+            printf("\nVelocidad: %d GHz", l[i].velocidad);
+            printf("\nAnio: %d", l[i].anio);
+            printf("\nCantidad: %d", l[i].cantidad);
+            printf("\nTipo de CPU: %s\n", l[i].tipo_cpu);
+        }
     }
 
 }
